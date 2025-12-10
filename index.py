df2815bbae2c2807de372ab24ce3f87434b90b9c
xxcchhuu <ashwin2004cv@gmail.com>
2025-12-10 14:22:41 +0530

    commmmmmmmi

--- index.py
+++ index.py
@@ -23,3 +23,25 @@ def access_token():
         return response.json()['access_token']
     except Exception as e:
         print("error in token ",e)
+        
+def get_new_release():
+    try:
+        token = access_token()
+        header = {'Authorization': f'Bearer {token}'}
+        Param = {'limit': 50}
+        response = requests.get('https://api.spotify.com/v1/browse/new-releases',
+                                headers=header, params=Param)
+        if response.status_code == 200:
+            # print(response.json())
+            data = response.json()
+            albums = data['albums']['items']
+            for i in albums:
+                a = {
+                    'album_name':i['name'],
+                    'Release_date' : i['release_date']
+                }
+                print(a)
+    except Exception as e:
+        print("Error in latest release data fetching ..",e)
+
+get_new_release()
